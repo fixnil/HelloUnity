9452072209419e96d2cb06496e307d72e865e0c1
fixnil <deping.du@gmail.com>
2023-03-27 00:24:18 +0800

    feat: 控制小球

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float _speed = 1;
+    public CharacterController _characterController;
+
     private void Start()
     {
-
+        _characterController ??= this.GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
+        print("xxx");
 
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis ("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            var vector = new Vector3(h, 0, v);
+
+            vector = this.transform.TransformDirection(vector);
+
+            _characterController.Move(vector * _speed);
+        }
     }
 }
