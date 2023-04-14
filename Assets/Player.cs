90e61ac9a2ca1869ab2a9e30ee2849ef069444cc
fixnil <deping.du@gmail.com>
2023-04-14 22:43:24 +0800

    chore: 编辑器版本升级

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,16 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float _speed = 1;
+    public float _speed = 10;
     public CharacterController _characterController;
-
-    private void Start()
-    {
-        if (_characterController == null)
-        {
-            _characterController = this.GetComponent<CharacterController>();
-        }
-    }
 
     private void Update()
     {
